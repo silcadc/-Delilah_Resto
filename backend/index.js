96ad182204c2f7c364fece6df61e1091e716b43c
Silena Dangond <silenadangond1@gmail.com>
2021-08-25 17:24:53 -0500

    creation endpoints for users

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,66 @@
 const express = require('express');
-//npm install body-parser
 let bodyParser = require('body-parser');
-const port = 3000;
-const cors = require('cors');
-//npm install jsonwebtoken
+let cors = require('cors');
 let jwt = require('jsonwebtoken');
-//npm install express-jwt
 let expressJwt = require('express-jwt');
+const helmet = require('helmet');
+const rateLimit = require("express-rate-limit");
 
+const sequelize = require('./database/conexion.js');
+const {getAllUsers, insertUsers, getLogin} = require("./database/users.js")
+
+const port = 3000;
 const server = express();
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use(bodyParser.json());
+
 server.listen(port, () => {
-    console.log("Servidor inicializado en puerto 3000")
+    console.log(`Server listeting on port ${port}`)
 });
 
-server.use(cors()) //para que permita que manden informacion 
-//desde afuera de este mismo puerto
+server.use(expressJwt({ secret: jwtClave, algorithms: ['sha1', 'RS256', 'HS256']}).unless({ path: ["/login"] }));
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+});
+server.use(limiter);
+
+server.get('/users', function (req, res) {
+    getAllUsers()
+    .then(users => {
+        res.status(200).send(users);
+    })
+    .catch(error => {
+        res.status(500).send(error)
+    })
+});
+
+server.post('/users ', (req, res) => {
+    const {
+        username, fullname, email, telephone, address, password, is_admin
+    } = req.body
+    let userInfo = [username, fullname, email, telephone, address, password, is_admin];
+    insertUsers(userInfo)
+    .then(response => {
+        res.status(200).send("user created successfully") 
+    })
+    .catch(error => {
+        res.status(500).send(error)
+    })
+});
+
+server.get('/users/login', function (req, res) {
+    const {
+        username, password
+    } = req.body
+    getUserLogin(username, password) 
+    .then(user => {
+        res.status(200).send(user);
+    })
+    .catch(error => {
+        res.status(500).send(error)
+    })
+});
